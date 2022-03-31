ffec5c413ee9016aa78673f012097e3a901e30ce
Marcin Fabrykowski <git@fabrykowski.pl>
2022-03-31 08:17:03 +0200

    set auth endpoint in helm - closes #219
    Signed-off-by: Marcin Fabrykowski <git@fabrykowski.pl>

--- nginx/entry.d/update_api.sh
+++ nginx/entry.d/update_api.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ -n "$APIURL" ]; then
     echo "Updating API_URL to $APIURL"
-    sed -i "s/window.API_URL =.*/window.API_URL = \"$APIURL\"/" /usr/share/nginx/html/env_vars.js
+    sed -i "s,window.API_URL =.*,window.API_URL = \"$APIURL\"," /usr/share/nginx/html/env_vars.js
 fi
 if [ -n "$AUTHURL" ]; then
     echo "Updating AUTH_URL to $AUTHURL"
-    sed -i "s/window.AUTH_URL =.*/window.AUTH_URL = \"$AUTHURL\"/" /usr/share/nginx/html/env_vars.js
+    sed -i "s,window.AUTH_URL =.*,window.AUTH_URL = \"$AUTHURL\"," /usr/share/nginx/html/env_vars.js
 fi
 if [ -n "$AUTHSECRET" ]; then
     echo "Updating AUTH_SECRET to $AUTHSECRET"
