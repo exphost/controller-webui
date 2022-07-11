2960c38dfa96df8bd72dc7c6e26be4cea4db1bf9
toleksa <toleksa@gmail.com>
2022-07-12 01:48:06 +0200

    basic logout mechanism - refs #341

---
+++ app/src/pages/LogoutPage.js
@@ -0,0 +1,13 @@
+import { useCookies } from 'react-cookie'
+
+export function LogoutPage () {
+  localStorage.removeItem('oauth2_state')
+  localStorage.removeItem('AUTH_URL')
+  localStorage.removeItem('AUTH_CLIENT_ID')
+  localStorage.removeItem('AUTH_SECRET')
+  return (
+    <h1>Logged out</h1>
+    <a href="https://www.exphost.pl/console">Login again</a>
+  )
+}
+
