d6f7db49e288929056b34e68dd7d934ec6e2d59c
toleksa <toleksa@gmail.com>
2022-07-11 20:47:58 +0200

    use Link

--- app/src/components/apps/appNginxList.jsx
+++ app/src/components/apps/appNginxList.jsx
@@ -15,7 +15,7 @@ function AppNginxList (props) {
         <td>
           { app.fqdns
             ? <ul>
-                { app.fqdns.map((fqdn, index) => <li key={index}> <a target='_blank' href={fqdn}>{fqdn}</a></li>)}
+                { app.fqdns.map((fqdn, index) => <li key={index}> <Link to={fqdn}>{fqdn}</Link></li>)}
               </ul>
             : '' }
         </td>
