9603b13523f906806793e1ac0b1217c108a969fd
toleksa <toleksa@gmail.com>
2022-07-12 02:15:58 +0200

    Adjacent JSX elements must be wrapped in an enclosing tag - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -6,8 +6,10 @@ export function LogoutPage () {
   localStorage.removeItem('AUTH_CLIENT_ID')
   localStorage.removeItem('AUTH_SECRET')
   return (
-    <h1>Logged out</h1>
-    <p><a href="https://www.exphost.pl/console">Login again</a></p>
+    <div>
+      <h1>Logged out</h1>
+      <a href="https://www.exphost.pl/console">Login again</a>
+    </div>
   )
 }
 
