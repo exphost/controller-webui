97a77dabb69850d4edc4d67fe1ac58cc7cc51a63
toleksa <toleksa@gmail.com>
2022-07-12 10:29:38 +0200

    eslist3 - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { useCookies } from 'react-cookie'
 
 export function LogoutPage () {
-  const [removeCookie] = useCookies()
+  const [cookies, setCookie, removeCookie] = useCookies()
+  gupi_eslist = cookies
+  bardzo_gupi_eslist = setCookie()
   removeCookie('accessToken', { path: '/' })
   removeCookie('refresh_token', { path: '/' })
   return (
