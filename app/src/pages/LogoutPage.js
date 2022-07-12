17a5873f81296343068a4a9f4b1795fa4f0fcaef
toleksa <toleksa@gmail.com>
2022-07-12 03:03:07 +0200

    destroy cookies - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -1,14 +1,13 @@
 import React from 'react'
+import { useCookies } from 'react-cookie'
 
 export function LogoutPage () {
-  localStorage.removeItem('oauth2_state')
-  localStorage.removeItem('AUTH_URL')
-  localStorage.removeItem('AUTH_CLIENT_ID')
-  localStorage.removeItem('AUTH_SECRET')
+  removeCookie('accessToken')
+  removeCookie('refresh_token')
   return (
     <div>
       <h1>Logged out</h1>
-      <a href="https://www.exphost.pl/console">Login again</a>
+      <a href="/console">Login again</a>
     </div>
   )
 }
