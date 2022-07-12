b0ff18c76163d4582801d129359d6d03de0b7f1a
toleksa <toleksa@gmail.com>
2022-07-12 03:32:03 +0200

    const - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useCookies } from 'react-cookie'
 
 export function LogoutPage () {
-  const [removeCookie] = useCookies()
+  const [cookies, setCookie, removeCookie] = useCookies()
   removeCookie('accessToken', { path: '/' })
   removeCookie('refresh_token', { path: '/' })
   return (
