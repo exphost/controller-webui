de2b362b1fb8f2f2e6c6d186cd3ac5763725a6b5
toleksa <toleksa@gmail.com>
2022-07-12 03:17:18 +0200

    eslint - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -3,8 +3,8 @@ import { useCookies } from 'react-cookie'
 
 export function LogoutPage () {
   const [removeCookie] = useCookies()
-  removeCookie('accessToken',{ path: '/' })
-  removeCookie('refresh_token',{ path: '/' })
+  removeCookie('accessToken', { path: '/' })
+  removeCookie('refresh_token', { path: '/' })
   return (
     <div>
       <h1>Logged out</h1>
