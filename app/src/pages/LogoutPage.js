c6ec495106f8ac217208cde1aa535bae80f0f3bf
toleksa <toleksa@gmail.com>
2022-07-12 11:01:08 +0200

    removie cookies and setCookie() - refs #341

--- app/src/pages/LogoutPage.js
+++ app/src/pages/LogoutPage.js
@@ -3,10 +3,8 @@ import { useCookies } from 'react-cookie'
 
 export function LogoutPage () {
   const removeCookie = useCookies()[2]
-  setCookie('accessToken', '', { path: '/' })
   removeCookie('accessToken', { path: '/' })
   removeCookie('refresh_token', { path: '/' })
-  console.log(cookies)
   return (
     <div>
       <h1>Logged out</h1>
