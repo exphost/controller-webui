7cbb582da2801a5bf8e2ce94576bc7ba9d432a19
Marcin Fabrykowski <git@fabrykowski.pl>
2023-11-23 10:17:27 +0100

    fix: add missing apps menu on sidemenu - closes #474
    Signed-off-by: Marcin Fabrykowski <git@fabrykowski.pl>

--- app/src/components/sidemenu.js
+++ app/src/components/sidemenu.js
@@ -58,6 +58,9 @@ function SideMenu (props) {
           <Nav.Item>
             <Nav.Link as={Link} to="/apps/emails" eventKey="emails">Emails</Nav.Link>
           </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/apps/app" eventKey="app">Apps</Nav.Link>
+          </Nav.Item>
           <hr/>
           <Nav.Item>
             <Nav.Link as={Link} to="/apps/domain">Domain</Nav.Link>
