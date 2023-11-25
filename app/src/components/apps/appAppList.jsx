ff46e81dbc92408e171ff5e6d41657f722b6d90d
Marcin Fabrykowski <git@fabrykowski.pl>
2023-11-25 15:09:25 +0100

    improve: add list of apps - closes #464
    Signed-off-by: Marcin Fabrykowski <git@fabrykowski.pl>

--- app/src/components/apps/appAppList.jsx
+++ app/src/components/apps/appAppList.jsx
@@ -1,15 +1,65 @@
-import React from 'react'
-// import React, { useState, useEffect } from 'react'
-// import { Table } from 'react-bootstrap'
-// import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { Table } from 'react-bootstrap'
+import axios from 'axios'
 import PropTypes from 'prop-types'
 
 function AppAppList (props) {
-  return <div>AppAppList</div>
+  const [apps, setApps] = useState(<tr><td>0</td><td span="2">Loading</td></tr>)
+
+  function makeTable (content) {
+    setApps(content.map((app, index) => (
+      <tr key={index}>
+        <td>{ index }</td>
+        <td>{ app }</td>
+      </tr>
+    )))
+  }
+
+  function loadApps () {
+    const requestOptions = {
+      url: window.API_URL + '/api/apps/v1/app/?org=' + props.org,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('access_token')
+      },
+      responseType: 'json'
+    }
+    axios
+      .request(requestOptions)
+      .then(function (response) {
+        const res = response.data // Response received from the API
+        makeTable(res)
+        return 0
+      })
+      .catch(function (err) {
+        console.log('submit failed', err.response)
+        // alert("Submit failed")
+      })
+  }
+
+  useEffect(() => {
+    loadApps()
+  }, [props.org, props.refreshList])
+
+  return (
+        <React.Fragment>
+        <Table striped bordered hover>
+          <thead><tr>
+            <th>Id</th>
+            <th>Name</th>
+          </tr></thead>
+          <tbody>
+          {apps}
+          </tbody>
+        </Table>
+        </React.Fragment>
+  )
 }
 
 AppAppList.propTypes = {
-  org: PropTypes.string.isRequired
+  org: PropTypes.string.isRequired,
+  refreshList: PropTypes.bool.isRequired
 }
 
 export default AppAppList
---
+++ app/src/components/apps/appAppList.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, screen, waitFor, act } from '@testing-library/react'
+import AppAppList from './appAppList'
+import nock from 'nock'
+
+beforeEach(() => {
+  nock('http://localhost:8080')
+    .defaultReplyHeaders({
+      'access-control-allow-origin': '*',
+      'access-control-allow-credentials': 'true',
+      'access-control-allow-headers': 'Authorization'
+    })
+    .persist()
+    .intercept('/api/apps/v1/app/', 'OPTIONS')
+    .query(true)
+    .reply(200, null)
+    .persist()
+    .persist()
+    .get('/api/apps/v1/app/')
+    .query(true)
+    .reply(200, [
+      'app1', 'app2'
+    ])
+    .persist()
+})
+
+test('list app apps', async () => {
+  window.API_URL = 'http://localhost:8080'
+  act(() => {
+    render(<AppAppList org='test-org'/>)
+  })
+  await waitFor(() => expect(screen.getByText('app1')).toBeInTheDocument)
+  await waitFor(() => expect(screen.getByText('app2')).toBeInTheDocument)
+})
